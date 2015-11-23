64d48ad86c30fd4a66be92311fbd70a81148c055
AlexBowring <maullz@itadmins-MacBook-Pro.local>
2015-11-23 20:12:30 +0000

    ALL FSL FWE p005 empty files replaced with non-empty ones and .feat directory removed. SPM voxelwise .m script added for testing

---
+++ spm_voxelwise_p0001/VUncorrectedpdf.m
@@ -0,0 +1,15 @@
+%-----------------------------------------------------------------------
+% Job saved on 18-Nov-2015 10:20:32 by cfg_util (rev $Rev: 6460 $)
+% spm SPM - SPM12 (6470)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.results.spmmat = {'/storage/essicd/data/NIDM-Ex/Testing/ds000052/RESULTS/Sub01/SPM.mat'};
+matlabbatch{1}.spm.stats.results.conspec.titlestr = '';
+matlabbatch{1}.spm.stats.results.conspec.contrasts = Inf;
+matlabbatch{1}.spm.stats.results.conspec.threshdesc = 'none';
+matlabbatch{1}.spm.stats.results.conspec.thresh = 0.001;
+matlabbatch{1}.spm.stats.results.conspec.extent = 0;
+matlabbatch{1}.spm.stats.results.conspec.mask.none = 1;
+matlabbatch{1}.spm.stats.results.units = 1;
+matlabbatch{1}.spm.stats.results.print = 'pdf';
+matlabbatch{1}.spm.stats.results.write.none = 1;
