dacedc37272ac4de2f0bcb0016e0d3d32743b768
Alex Bowring <A.Bowring@warwick.ac.uk>
2015-12-07 11:39:36 +0000

    Modified spm conjunction and partial conjunction scripts using updated version of SPM

--- spm_conjunction/batch.m
+++ spm_conjunction/batch.m
@@ -1,6 +1,6 @@
 %-----------------------------------------------------------------------
-% Job saved on 27-Nov-2015 14:02:53 by cfg_util (rev $Rev: 6460 $)
-% spm SPM - SPM12 (6470)
+% Job saved on 07-Dec-2015 11:33:29 by cfg_util (rev $Rev: 6460 $)
+% spm SPM - SPM12 (12.1)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
 matlabbatch{1}.spm.stats.fmri_spec.dir = {'/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub01'};
@@ -252,6 +252,7 @@ matlabbatch{4}.spm.stats.results.conspec.contrasts = [1 2];
 matlabbatch{4}.spm.stats.results.conspec.threshdesc = 'none';
 matlabbatch{4}.spm.stats.results.conspec.thresh = 0.05;
 matlabbatch{4}.spm.stats.results.conspec.extent = 0;
+matlabbatch{4}.spm.stats.results.conspec.conjunction = 1;
 matlabbatch{4}.spm.stats.results.conspec.mask.none = 1;
 matlabbatch{4}.spm.stats.results.units = 1;
 matlabbatch{4}.spm.stats.results.print = 'pdf';
