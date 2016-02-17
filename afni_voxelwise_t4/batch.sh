ac5424deb813c5f6f433c9b68c906dd90dc0b24a
RuthPauli <r.pauli@warwick.ac.uk>
2016-02-17 10:19:15 +0000

    AFNI T>=4

--- afni_voxelwise_t4/batch.sh
+++ afni_voxelwise_t4/batch.sh
@@ -1,1 +1,1 @@
-3dclust -1Dformat -nosum -1dindex 2 -1tindex 2 -2thresh -4 4 -inmask -dxyz=1 -savemask Clust_mask 1.01 2 ./afni_voxelwise_t4/sub_001.results/./stats.sub_001+tlrc.HEAD
+3dclust -1Dformat -nosum -1dindex 5 -1tindex 5 -2thresh -4 4 -inmask -dxyz=1 -savemask Clust_mask 1.01 2 ./afni_voxelwise_t4/sub_001.results/stats.sub_001+tlrc.HEAD
