f79d65572805aa087fd6a32211bbac263c406791
RuthPauli <r.pauli@warwick.ac.uk>
2016-03-01 10:02:52 +0000

    fix: nn3, stimulus duration

--- afni_cluster_nn3/batch.sh
+++ afni_cluster_nn3/batch.sh
@@ -1,1 +1,1 @@
-3dclust -1Dformat -nosum -1dindex 5 -1tindex 5 -2thresh -3.415 3.415 -inmask -dxyz=1 -savemask Clust_mask 1.75 2 ./afni_cluster_nn3/sub_001.results/stats.sub_001+tlrc.HEAD
+3dclust -1Dformat -nosum -1dindex 2 -1tindex 2 -2thresh -3.413 3.413 -inmask -dxyz=1 -savemask Clust_mask 1.75 2 ./afni_cluster_nn3/sub_001.results/stats.sub_001+tlrc.HEAD
