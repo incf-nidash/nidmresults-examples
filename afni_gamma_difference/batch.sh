2d60aaf50cad239a26d4115e5815ff308c471bd1
cmaumet <c.m.j.maumet@warwick.ac.uk>
2016-04-13 09:42:20 +0100

    thresholded map at p<0.001 unc.

--- afni_gamma_difference/batch.sh
+++ afni_gamma_difference/batch.sh
@@ -1,1 +1,1 @@
-3dclust -1Dformat -nosum -1dindex 2 -1tindex 2 -2thresh -3.415 3.415 -inmask -dxyz=1 -savemask Clust_mask 1.01 2 ./subject_results/group.DS0011/sub_001.results/stats.sub_001+tlrc.HEAD
+3dclust -1Dformat -nosum -1dindex 2 -1tindex 2 -2thresh -3.415 3.415 -inmask -dxyz=1 -savemask Clust_mask 1.01 2 ./subject_results/group.DS0011/subj.sub_001/sub_001.results/stats.sub_001+tlrc.HEAD
