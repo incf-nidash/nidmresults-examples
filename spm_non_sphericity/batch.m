6c0a9134e967fb861972ffe69da8de56e96a9782
AlexBowring <A.bowring@warwick.ac.uk>
2016-07-04 11:19:39 +0100

    spm_non_sphericity input files directory created and batch amended to use these files rather than from an external directory

--- spm_non_sphericity/batch.m
+++ spm_non_sphericity/batch.m
@@ -1,6 +1,6 @@
 %-----------------------------------------------------------------------
-% Job saved on 08-Dec-2015 10:52:04 by cfg_util (rev $Rev: 6460 $)
-% spm SPM - SPM12 (6470)
+% Job saved on 04-Jul-2016 11:13:21 by cfg_util (rev $Rev: 6460 $)
+% spm SPM - SPM12 (6685)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
 matlabbatch{1}.spm.stats.factorial_design.dir = {'/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Group/Informed'};
@@ -13,58 +13,58 @@ matlabbatch{1}.spm.stats.factorial_design.des.fd.fact.ancova = 0;
 matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(1).levels = 1;
 %%
 matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(1).scans = {
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub01/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub02/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub03/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub04/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub05/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub06/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub07/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub08/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub09/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub10/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub11/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub12/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub13/CanonicalHRF/con_0001.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub14/CanonicalHRF/con_0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub01_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub02_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub03_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub04_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub05_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub06_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub07_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub08_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub09_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub10_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub11_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub12_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub13_con0001.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub14_con0001.nii,1'
                                                                    };
 %%
 matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(2).levels = 2;
 %%
 matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(2).scans = {
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub01/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub02/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub03/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub04/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub05/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub06/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub07/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub08/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub09/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub10/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub11/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub12/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub13/CanonicalHRF/con_0005.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub14/CanonicalHRF/con_0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub01_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub02_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub03_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub04_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub05_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub06_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub07_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub08_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub09_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub10_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub11_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub12_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub13_con0005.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub14_con0005.nii,1'
                                                                    };
 %%
 matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(3).levels = 3;
 %%
 matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(3).scans = {
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub01/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub02/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub03/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub04/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub05/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub06/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub07/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub08/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub09/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub10/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub11/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub12/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub13/CanonicalHRF/con_0009.nii,1'
-                                                                   '/storage/essicd/data/NIDM-Ex/Testing/ds000006/RESULTS/Sub14/CanonicalHRF/con_0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub01_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub02_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub03_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub04_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub05_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub06_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub07_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub08_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub09_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub10_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub11_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub12_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub13_con0009.nii,1'
+                                                                   '/storage/essicd/data/NIDM-Ex/BIDS_Data/RESULTS/TEST/nidmresults-examples/spm_non_sphericity/input/sub14_con0009.nii,1'
                                                                    };
 %%
 matlabbatch{1}.spm.stats.factorial_design.des.fd.contrasts = 1;
@@ -90,6 +90,7 @@ matlabbatch{4}.spm.stats.results.conspec.contrasts = 1;
 matlabbatch{4}.spm.stats.results.conspec.threshdesc = 'none';
 matlabbatch{4}.spm.stats.results.conspec.thresh = 0.001;
 matlabbatch{4}.spm.stats.results.conspec.extent = 0;
+matlabbatch{4}.spm.stats.results.conspec.conjunction = 1;
 matlabbatch{4}.spm.stats.results.conspec.mask.none = 1;
 matlabbatch{4}.spm.stats.results.units = 1;
 matlabbatch{4}.spm.stats.results.print = 'pdf';
